36dcc49bff5e92b84f0ff10b1384b26c232d191c
BilgeKaganOzkan <147448844+BilgeKaganOzkan@users.noreply.github.com>
2024-03-18 22:08:58 +0300

    Update soapClient.py

--- soapClient.py
+++ soapClient.py
@@ -6,13 +6,14 @@ wsdl_url = 'currency.wsdl'
 # SOAP istemcisini oluştur
 client = Client(wsdl=wsdl_url)
 
-# SOAP isteği için gerekli veri
 soap_request_data = {
-    'country': 'USA'  # Örnek bir ülke adı
+    'country': 'USA'
 }
 
-# SOAP isteğini gönder
-response = client.service.GetCurrencyByCountry(soap_request_data)
+# Mesaj oluştur
+request_message = client.get_element('{http://is550.soap.com}GetCurrencyByCountryRequest')
+message = request_message(country=soap_request_data['country'])
+response = client.service.GetCurrencyByCountry(message)
 
 # Yanıtı yazdır
 print("Received SOAP Response:")
